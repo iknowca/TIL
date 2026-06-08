8ff1ac8b9f347f3c16d3edab94948d91694dc47f
iknow <iknowca@gmail.com>
2026-06-08 23:13:14 +0900

    MNIST 데이터셋 로드 및 배치 샘플 코드 추가

---
+++ DeepLearning/DeepLearningFromScratchVol1/chapter3/functions.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def sigmoid(x):
+    x = np.asarray(x)
+    return np.where(
+        x >= 0,
+        1 / (1 + np.exp(-x)),
+        np.exp(x) / (1 + np.exp(x)),
+    )
+
+def identity(x):
+    return x
+
+def softmax(x):
+    c = np.max(x)
+    exp_x = np.exp(x - c)
+    sum_exp_x = np.sum(exp_x)
+    return exp_x / sum_exp_x
+
