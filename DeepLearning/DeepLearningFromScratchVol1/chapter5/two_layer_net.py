bc31d5e8e9f56d1810e609c943a8184c6d13d40e
iknow <iknowca@gmail.com>
2026-06-14 12:46:03 +0900

    신경망 학습 예제 추가 및 관련 레이어 클래스 구현

---
+++ DeepLearning/DeepLearningFromScratchVol1/chapter5/two_layer_net.py
@@ -0,0 +1,70 @@
+import os, sys
+from collections import OrderedDict
+
+import numpy as np
+
+from affine_layer import AffineLayer
+from chapter5.softmax_with_loss_layer import SoftmaxWithLoss
+from common.gradient import numerical_gradient
+from relu_layer import ReLU
+
+sys.path.append(os.pardir)
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = AffineLayer(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = ReLU()
+        self.layers['Affine2'] = AffineLayer(self.params['W2'], self.params['b2'])
+        self.lastLayer = SoftmaxWithLoss()
+
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+
+        return x
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.lastLayer.forward(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        if t.ndim != 1: t = np.argmax(t, axis=1)
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
+    def gradient(self, x, t):
+        self.loss(x, t)
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+
+        return grads
