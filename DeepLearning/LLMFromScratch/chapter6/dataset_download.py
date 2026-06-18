4d014070c35d39f5310b379758f8818aa4032454
iknow <iknowca@gmail.com>
2026-06-18 10:47:59 +0900

    스팸 데이터 다운로드 및 전처리 스크립트 추가, pandas 종속성 업데이트.

---
+++ DeepLearning/LLMFromScratch/chapter6/dataset_download.py
@@ -0,0 +1,28 @@
+import urllib.request
+import zipfile
+import os
+from pathlib import Path
+
+url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
+zip_path = "sms_spam_collection.zip"
+extracted_path = "sms_spam_collection"
+data_file_path = Path(extracted_path) / "SMSSpamCollection.tsv"
+
+def download_and_unzip_spam_data(
+        url, zip_path, extracted_path, data_file_path):
+    if data_file_path.exists():
+        print(f"Data file already exists: {data_file_path}")
+        return
+
+    with urllib.request.urlopen(url) as response:
+        with open(zip_path, "wb") as out_file:
+            out_file.write(response.read())
+
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extracted_path)
+        original_file_path = Path(extracted_path) / "SMSSpamCollection"
+        os.rename(original_file_path, data_file_path.parent)
+        print(f"Data file saved to: {data_file_path}")
+
+if __name__ == "__main__":
+    download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
