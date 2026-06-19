0c4e8bd3df75b01313717c6939e7378be8f018a3
iknow <iknowca@gmail.com>
2026-06-19 15:05:11 +0900

    GPT 모델 파인튜닝: 훈련 루프 추가

--- DeepLearning/LLMFromScratch/chapter6/gpt_finetune.py
+++ DeepLearning/LLMFromScratch/chapter6/gpt_finetune.py
@@ -49,3 +49,46 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
         else:
             break
     return total_loss / num_batches
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(
+            train_loader, model, device, num_batches=eval_iter
+        )
+        val_loss = calc_loss_loader(
+            val_loader, model, device, num_batches=eval_iter
+        )
+    model.train()
+    return train_loss, val_loss
+
+def train_classifier_simple(
+        model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter
+):
+    train_losses, val_losses, train_accs, val_accs = [], [], [], []
+    examples_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            examples_seen += input_batch.shape[0]
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+
+                print(f"Epoch {epoch}, Step {global_step}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
+        train_accuracy = calc_accuracy_loader(train_loader, model, device)
+        val_accuracy = calc_accuracy_loader(val_loader, model, device)
+        train_accs.append(train_accuracy)
+        val_accs.append(val_accuracy)
+        print(f"Epoch {epoch}, Train Accuracy: {train_accuracy:.4f}, Val Accuracy: {val_accuracy:.4f}")
+
+    return train_losses, val_losses, train_accs, val_accs, examples_seen
