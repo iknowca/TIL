ffbc38b9e859a55796a143aed432c92ecb8dae56
iknow <iknowca@gmail.com>
2026-09-10 23:53:38 +0900

    챕터 4 예제 코드 추가: Padovan 수열 계산, 튜플 사용, Person 데이터 저장 및 출력

---
+++ Language/Rust/programming_rust/ch04/ex2_tuple/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    {
+        let point = Box::new((0.625, 0.5));
+        let label = format!("{:?}", point);
+        assert_eq!(label, "(0.625, 0.5)");
+    }
+}
---
+++ Language/Rust/programming_rust/ch04/ex3_person/src/main.rs
@@ -0,0 +1,24 @@
+struct Person {
+    name: String,
+    birth: i32,
+}
+
+fn main() {
+    let mut composers = Vec::new();
+    composers.push(Person {
+        name: "Palestrina".to_string(),
+        birth: 1525
+    });
+    composers.push(Person {
+        name: "Dowland".to_string(),
+        birth: 1525
+    });
+    composers.push(Person {
+        name: "Lully".to_string(),
+        birth: 1632
+    });
+
+    for composer in &composers {
+        println!("{}, born {}", composer.name, composer.birth);
+    }
+}
